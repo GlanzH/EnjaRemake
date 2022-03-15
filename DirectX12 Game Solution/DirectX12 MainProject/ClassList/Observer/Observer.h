d424b00cc857005d97e16bc3dfaf63d2daf90cbe
hikaru <opatorishia@gmail.com>
2022-03-15 15:20:56 +0900

    エラー修正完了

--- DirectX12 Game Solution/DirectX12 MainProject/ClassList/Observer/Observer.h
+++ DirectX12 Game Solution/DirectX12 MainProject/ClassList/Observer/Observer.h
@@ -1,17 +1,2 @@
 #pragma once
 
-#include"ClassList/Base.h"
-
-class PlayerBase : public Base
-{
-public:
-	PlayerBase() {};
-	~PlayerBase() {};
-	bool Initialize();
-	void LoadAssets();
-	int Update();
-	void Render();
-
-private:
-
-};
